196e25b06f3570ae86119cb5d1928b565714fdb2
Cyprien Dcunha <dcunha.cyprien@gmail.com>
2018-01-03 21:44:25 +0530

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,139 @@
+function showFollowingMenuOnTransition(){
+    $('.masthead').visibility({
+        once: false,
+        onBottomPassed: function() {
+            $('.fixed.menu').transition('fade in');
+        },
+        onBottomPassedReverse: function() {
+            $('.fixed.menu').transition('fade out');
+        }
+    });
+}
+
+function formatDate(isodate) {
+    var date = new Date(isodate);
+    var monthNames = [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+        "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+
+    return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
+
+function addProjectCards(){
+    for(let p in projects){
+        prepo = projects[p].repo;
+        pname = ("name" in projects[p])?projects[p].name:prepo;
+        plang = projects[p].language;
+        pdesc = projects[p].description;
+        ptool = ""
+
+        for(let tool of projects[p].tools){
+            ptool += "<li>" + tool + "</li>";
+        }
+
+        if(projects[p].type == "github"){
+            $.get("https://api.github.com/repos/ccd97/"  + prepo, function(json){
+                $('#udate-' + p).append("Updated on " + formatDate(json.pushed_at));
+                $('#stat-' + p).append(`<i class="empty star icon"></i>` + json.stargazers_count);
+                $('#stat-' + p).append(`<i class="fork icon"></i>` + json.forks_count);
+                $('#stat-' + p).append(`<a class="right floated" href="`
+                    + json.html_url +`">Visit <i class="github icon"></i></a>`);
+            });
+        }
+        else{
+            setTimeout(function(){
+                if("updated_at" in projects[p])
+                    $('#udate-' + p).append("Updated on " + projects[p].updated_at);
+                if("type" in projects[p] && projects[p].type == "gitlab"){
+                    repourl = "https://gitlab.com/ccd97/" + prepo;
+                    $('#stat-' + p).append(`<a class="right floated" href="`
+                    + repourl +`">Visit <i class="gitlab icon"></i></a>`);
+                }
+                else
+                    $('#stat-' + p).append(`<a class="right floated" href="`
+                            + projects[p].link +`">Visit Project</a>`);
+            }, 300);
+        }
+
+
+
+        cardhtml = `
+            <div class="ui fluid card">
+                <div class="content h150">
+                    <div class="header">` + pname + `
+                        <span class="right floated language-label ` + plang + `">
+                            ⚫&nbsp; ` + plang +`
+                        </span>
+                    </div>
+                    <div class="meta"><span class="date" id="udate-` + p +`"></span></div>
+                    <div class="description">` + pdesc +`</div>
+                </div>
+                <div class="content h130">
+                    <div class="description">
+                        Tools used : <br>
+                        <ul class="ui list">` + ptool +`</ul>
+                    </div>
+                </div>
+                <div class="extra content" id="stat-` + p +`"></div>
+            </div>`
+
+            $('#project-cards').append(cardhtml);
+    }
+}
+
+function addSkillCards(){
+
+    first_card = true;
+
+    for(let skillset of skills){
+        sstitle = skillset.title;
+        sscontent = ""
+
+        for(let scont of skillset.content){
+            sscontent += `<div class="description">` + scont.description;
+            sscontent += `<br><ul class="ui list">`;
+            for(let skill of scont.skill_list){
+                if(first_card) p_pos = "right center";
+                else p_pos = "left center"
+                sweak = skill.weak?` class="weak-skill" `:"";
+                spop = ("popup" in skill)?` data-tooltip="`+ skill.popup +`" data-position="` + p_pos +`" `:"";
+                sscontent += `<li><div ` + sweak + spop + `> ` + skill.name + "</div></li>";
+            }
+            sscontent += "</ul></div><br>"
+        }
+        cardhtml = `
+            <div class="ui fluid card">
+                <div class="content">
+                    <div class="header">` + sstitle + `</div> ` + sscontent +`
+                </div>
+            </div>`
+
+        $('#skill-cards').append(cardhtml);
+
+        if(first_card) first_card = false;
+    }
+}
+
+$(document).ready(function() {
+    showFollowingMenuOnTransition();
+    setMenuScroll();
+    addProjectCards();
+    addSkillCards();
+});
+
+function setMenuScroll(){
+    $('.menu .item').on('click', function(event){
+        var id = $(this).attr('href').replace('#', '')
+        var position = $('#' + id).offset().top;
+        $('html, body').animate({ scrollTop: position }, 500);
+        var menuitem = '.menu_' + id.split("_")[1]
+        $('.menu .item').removeClass('active');
+        $(menuitem).addClass('active');
+        event.preventDefault();
+    });
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# [My Portfolio](https://ccd97.github.io/)
+
+This portfolio was developed using [jQuery](http://jquery.com/) and [Semantic-UI](https://semantic-ui.com/)
